00aa58a707df9b422bb19a06d7132ece498d1b5b
Robert Curley <rjc03@d25rp1dvgq17im.amer.homedepot.com>
2016-11-01 15:05:47 -0400

    Initiated project

---
+++ src/main/java/com/homedepot/mm/po/allocation/AllocationTeamDataApplication.java
@@ -0,0 +1,12 @@
+package com.homedepot.mm.po.allocation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllocationTeamDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllocationTeamDataApplication.class, args);
+	}
+}
---
+++ src/main/java/com/homedepot/mm/po/allocation/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.homedepot.mm.po.allocation;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(AllocationTeamDataApplication.class);
+	}
+
+}
